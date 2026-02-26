5b7580e8fd72bf544054d565d47992f31e595089
Jeff Weeks <jweeks@neuraldk.org>
2026-02-26 08:35:37 -0500

    Revert "Add explicit panic handler"
    This reverts commit c0e92783944a013cd084113b9ecdbe1e9a7e5ac1.

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,7 @@
 #![no_std]
 
-use core::panic::PanicInfo;
-
 #[cfg(test)]
 extern crate std;
-
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
 
 pub mod config;
 pub mod module_list;
