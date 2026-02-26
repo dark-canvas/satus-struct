c0e92783944a013cd084113b9ecdbe1e9a7e5ac1
Jeff Weeks <jweeks@neuraldk.org>
2026-02-26 08:34:25 -0500

    Add explicit panic handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
 #![no_std]
 
+use core::panic::PanicInfo;
+
 #[cfg(test)]
 extern crate std;
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
 
 pub mod config;
 pub mod module_list;
